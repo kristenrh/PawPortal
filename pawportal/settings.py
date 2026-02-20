d4da1cf64403d7b2a5c1190297bce1e9fdc7ef2f
AgentHOOT <ratha2298@my.uwstout.edu>
2026-02-20 16:41:45 -0600

    connnection to postfresql test

--- pawportal/settings.py
+++ pawportal/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'pawportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PawPortal',
+        'USER': "postgres",
+        'PASSWORD': 'Minecraft@101',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
