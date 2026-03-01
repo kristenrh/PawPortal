780df96913fbc75eb77ce13b9c2f7d1870a38e6a
XtremeMonkey <l8rr8r@gmail.com>
2026-02-28 18:19:51 -0600

    Broke kennel page, so commented out parts that caused the error

--- pawportalapp/views.py
+++ pawportalapp/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Animal, AnimalLocation
+#from .models import Animal, AnimalLocation
 
 def dashboard(request):
     return render(request, "dashboard.html")
 
 def kennel(request):
-    animals = Animal.objects.all()
-    kennel = AnimalLocation.objects.all()
-    return render(request, "kennel.html", {"animals": animals, "kennel": kennel})
+    #animals = Animal.objects.all()
+    #kennel = AnimalLocation.objects.all()
+    return render(request, "kennel.html")
 
 def socialization(request):
     return render(request, "socialization.html")
