40f2d678c184618464369b2e5e650cca1e36b457
AgentHOOT <ar.adam.rath@gmail.com>
2026-03-09 16:47:09 -0500

    a basic connection

--- pawportalapp/models.py
+++ pawportalapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-class Product(models.Model):
-    uname = models.CharField(max_length=100)
- #  price = models.DecimalField(max_digits=10, decimal_places=2)
-   #  created_at = models.DateTimeField(auto_now_add=True)
+class animal(models.Model):
+    animalname = models.CharField(max_length=100)
+    animalid = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = "animal"
+
 
     def __str__(self):
-        return self.name
+        return self.animalname
--- pawportalapp/views.py
+++ pawportalapp/views.py
@@ -1,12 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Product
+from .models import animal
 
 def dashboard(request):
     return render(request, "dashboard.html")
-
-def kennel(request):
-    return render(request, "kennel.html")
 
 def socialization(request):
     return render(request, "socialization.html")
@@ -14,10 +11,10 @@ def socialization(request):
 def adoption(request):
     return render(request, "adoption.html")
 
-def product_list(request):
+def kennel(request):
     try:
-        products = Product.objects.all()  # Fetch all products
+        products = animal.objects.all()  # Fetch all products
     except Exception as e:
         products = []
         print(f"Database error: {e}")
-    return render(request, 'product_list.html', {'products': products})
+    return render(request, 'kennel.html', {'products': products})
