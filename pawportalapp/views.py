d4039bbd9440b0bd5616243e88d8a2c40789a03c
pantzlaffw1606 <144815749+pantzlaffw1606@users.noreply.github.com>
2026-03-09 15:44:37 -0500

    Importing Loader

--- pawportalapp/views.py
+++ pawportalapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 def dashboard(request):
     loginPage = loader.get_template('LoginPage.html')
