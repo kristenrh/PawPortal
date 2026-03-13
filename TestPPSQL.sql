007051a1eb63670e6af79059e935287718876770
XtremeMonkey <l8rr8r@gmail.com>
2026-03-13 16:11:17 -0500

    Modify Person table structure and data
    Added person_role and Status columns to Person table. Updated INSERT statements for Person table.

--- TestPPSQL.sql
+++ TestPPSQL.sql
@@ -1,54 +1,79 @@
-CREATE TABLE Animal (
+ CREATE TABLE Animal (
+
 AnimalName VARCHAR(25),
+
 AnimalSpecies VARCHAR(100),
+
 AnimalID SERIAL PRIMARY KEY,
+
 AnimalLocation VARCHAR(5),
+
 LastFed TIMESTAMP,
+
 LastWalk TIMESTAMP,
+
 AnimalAge SMALLINT,
+
 BehaviorType VARCHAR(25)
+
 );
+
 INSERT INTO Animal(
+
 AnimalName,
+
 AnimalSpecies,
+
 AnimalLocation,
+
 LastFed,
+
 LastWalk,
+
 AnimalAge,
+
 BehaviorType
+
 )
+
 VALUES
+
 ('Ruby','Mixed Breed','A01','2026-02-19 9:30','2026-02-19 14:30',5,'Aggressive'),
+
 ('Mozart', 'Cavalier King Charles Spaniel', 'B01','2026-02-19 9:30', '2026-02-19 14:30',6, 'Energetic'),
+
 ('Willow', 'Beagle', 'C01', '2026-02-19 9:30', '2026-02-19 14:30', 12, 'Loner'),
+
 ('Jack', 'Cavalier King Charles Spaniel', 'D01', '2026-02-19 9:30', '2026-02-19 14:30', 10, 'Playful'),
+
 ('Mini Mae', 'Toy Shih Tzu','E01', '2026-02-19 9:30', '2026-02-19 14:30', 14, 'Vocal');
 
+
 CREATE TABLE Person(
+
 PersonKey SERIAL PRIMARY KEY,
+
 FirstName VARCHAR(25),
-LastName VARCHAR(25)
+
+LastName VARCHAR(25),
+
+person_role VARCHAR(10) CHECK(Pers IN ('Staff', 'Volunteer', 'Adopter')),
+
+Status VARCHAR(10)
+
 );
+
 
 INSERT INTO Person
-(Firstname, Lastname)
+
+(FirstName, LastName, PersonRole)
+
 VALUES
-('Adam', 'Rath'),
-('Daniel', 'Schultz'),
-('Will', 'Pantzlaff'),
-('Kristen' 'Henningfeld');
 
-CREATE TABLE Staff(
-StaffKey SERIAL PRIMARY KEY,
-PersonKey INTEGER REFERENCES Person(PersonKey)
-);
+('Firstname', 'lastname', 'Volunteer'),
 
-CREATE TABLE Volunteer(
-PersonKey INTEGER REFERENCES Person(PersonKey),
-VolunteerID SERIAL PRIMARY KEY
-);
+('Firstname', 'lastname', 'Staff'),
 
-CREATE TABLE Adopter(
-PersonKey INTEGER REFERENCES Person(PersonKey),
-AdopterID SERIAL PRIMARY KEY
-);
+('Firstname', 'lastname', 'Volunteer'),
+
+('Firstname', 'lastname', 'Staff'); 
